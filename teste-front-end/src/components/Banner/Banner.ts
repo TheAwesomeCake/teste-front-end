2ae08ad81182c21de9301ce08446160f9b8633d3
TheAwesomeCake <luc45.2004@gmail.com>
2026-03-02 19:56:44 -0300

    showcase made

---
+++ teste-front-end/src/components/Banner/Banner.ts
@@ -0,0 +1,14 @@
+import './Banner.scss'
+import BannerImage from '../../assets/Banner/Banner.png'
+
+export function Banner() {
+  return `
+    <div class="banner" style="background-image: url(${BannerImage})">
+      <div class="banner-overlay">
+        <h1>Venha conhecer nossos produtos</h1>
+        <h2><span>50% Off</span> nos produtos</h2>
+        <button>Ver produto</button>
+      </div>
+    </div>
+  `
+}
--- teste-front-end/src/main.ts
+++ teste-front-end/src/main.ts
@@ -1,6 +1,8 @@
 import './style.scss'
 import { Header } from './components/Header/Header'
+import { Banner } from './components/Banner/Banner'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   ${Header()}
+  ${Banner()}
 `
